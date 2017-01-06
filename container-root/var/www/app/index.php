ead904ba6df9c5a3a7af486a64442dbb71951521
Gleb Poljakov <gleb.poljakov@gmail.com>
2017-01-07 01:36:18 +0300

    2017-01-07 01:36

--- container-root/var/www/app/index.php
+++ container-root/var/www/app/index.php
@@ -17,17 +17,16 @@
 working. Further configuration is required.</p>
 
 <h2>The gleb.poljakov/alpine-nginx-php5 variant.</h2>
-<?php echo "<p>if you see this text, the PHP is successfully configured and working. Information about PHP on this server is accesseble <a href='phpinfo.php'>there</a>. Further configuration is required.</p>" ?>
+<?php echo "<p>if you see this text, the PHP is successfully configured and working. Information about PHP on this server is accesseble <a href=\"phpinfo.php\">there</a>. Further configuration is required.</p>" ?>
 
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
+<h2>Documentation</h2>
+
+<p>For online documentation and support please refer to <a href="http://nginx.org/">nginx.org</a>.<br/>
 Commercial support is available at
 <a href="http://nginx.com/">nginx.com</a>.</p>
 
 <p>For online documentation specific to the smebberson/alpine-nginx,<br/>
 please refer to <a href="https://github.com/smebberson/docker-alpine/alpine-nginx">smebberson/docker-alpine</a>.</p>
-
-<p>
 
 <p><em>Thank you for using nginx.</em></p>
 </body>
