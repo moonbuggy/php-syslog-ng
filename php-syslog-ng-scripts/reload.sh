fae725614bf77b216f575a415854f4bb1b0901c5
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2020-01-01 21:12:36 +1100

    archive

--- php-syslog-ng-scripts/reload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/mysql -uroot -pPASSWORD_ROOT_USER -e'use syslog; delete from search_cache;'
-/usr/bin/php /var/www/scripts/reloadcache.php >> /var/log/logzilla/reloadcache.log
-
